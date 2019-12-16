ea894d4e74a18654cafa52731d5d10502ea02223
Anna Rzucidło <arzucidlo1@gmail.com>
2019-12-16 23:44:15 +0100

    sample

---
+++ adapter/src/main/java/CsvFormatter.java
@@ -0,0 +1,8 @@
+public class CsvFormatter implements CsvFormattable{
+    @Override
+    public String formatCsvText(String text){
+        String formattedText=text.replace(".",",");
+        return formattedText;
+    }
+}
+
---
+++ adapter/src/main/java/TextFormattable.java
@@ -0,0 +1,4 @@
+public interface TextFormattable {
+    String formatText(String text);
+}
+
